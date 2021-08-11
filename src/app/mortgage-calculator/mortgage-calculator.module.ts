bed742b2eb4254ef75a198b3ea02dbc170026722
Baadier Sydow <bsydow@gmail.com>
2021-08-11 23:09:31 +0200

    Module to host all calc functionality

--- src/app/mortgage-calculator/mortgage-calculator.module.ts
+++ src/app/mortgage-calculator/mortgage-calculator.module.ts
@@ -1,4 +1,21 @@
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+
+import { MortgageCalculatorFormComponent } from './components/mortgage-calculator-form/mortgage-calculator-form.component';
+import { MonthlyRepaymentComponent } from './components/monthly-repayment/monthly-repayment.component';
 import { HouseDisplayComponent } from './components/house-display/house-display.component';
+import { PriceRangeComponent } from './components/price-range/price-range.component';
+
 import { MortgageCalculatorService } from './services/mortgage-calculator.service';
+
+@NgModule({
   declarations: [MortgageCalculatorFormComponent, HouseDisplayComponent, PriceRangeComponent, MonthlyRepaymentComponent],
+  imports: [CommonModule, IonicModule, FormsModule],
+  exports: [MortgageCalculatorFormComponent],
   providers: [MortgageCalculatorService]
+})
+export class MortgageCalculatorModule {
+  constructor() {}
+}
