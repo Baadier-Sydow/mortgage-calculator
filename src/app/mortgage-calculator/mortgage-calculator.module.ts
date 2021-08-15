8d060a7ba1f45b11a523612ee189669a5b01ae02
Baadier Sydow <bsydow@gmail.com>
2021-08-15 23:17:37 +0200

    Remove unused components

--- src/app/mortgage-calculator/mortgage-calculator.module.ts
+++ src/app/mortgage-calculator/mortgage-calculator.module.ts
@@ -4,14 +4,12 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 
 import { MortgageCalculatorFormComponent } from './components/mortgage-calculator-form/mortgage-calculator-form.component';
-import { MonthlyRepaymentComponent } from './components/monthly-repayment/monthly-repayment.component';
 import { HouseDisplayComponent } from './components/house-display/house-display.component';
-import { PriceRangeComponent } from './components/price-range/price-range.component';
 
 import { MortgageCalculatorService } from './services/mortgage-calculator.service';
 
 @NgModule({
-  declarations: [MortgageCalculatorFormComponent, HouseDisplayComponent, PriceRangeComponent, MonthlyRepaymentComponent],
+  declarations: [MortgageCalculatorFormComponent, HouseDisplayComponent],
   imports: [CommonModule, IonicModule, FormsModule],
   exports: [MortgageCalculatorFormComponent],
   providers: [MortgageCalculatorService]
