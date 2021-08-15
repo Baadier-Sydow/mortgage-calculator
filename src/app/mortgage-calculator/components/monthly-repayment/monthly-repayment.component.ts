6be864f4010e941ff61fdd9a0960ba6beb60d2e2
Baadier Sydow <bsydow@gmail.com>
2021-08-15 22:15:04 +0200

    Removed unneeded functionality

--- src/app/mortgage-calculator/components/monthly-repayment/monthly-repayment.component.ts
+++ src/app/mortgage-calculator/components/monthly-repayment/monthly-repayment.component.ts
@@ -6,37 +6,16 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./monthly-repayment.component.scss'],
 })
 export class MonthlyRepaymentComponent implements OnInit {
-  @Input() defaultTermAmount: number;
-  @Input() defaultTermMax: number;
   @Output() termChange = new EventEmitter<number>();
 
-  termAmount: number;
-  priceMin = 10000;
-  priceMax: number;
   rangeDebounce = 0;
 
   constructor() {}
 
-  ngOnInit() {
-    this.termAmount = this.defaultTermAmount;
-    this.priceMax = this.defaultTermMax;
-  }
+  ngOnInit() { }
 
   updateTerm(updatedTerm) {
-    this.termAmount = updatedTerm.detail.value;
-    this.validateMaxTermRange();
-    this.termChange.emit(this.termAmount);
-  }
-
-  validateMaxTermRange() {
-    // if (this.price > this.priceMax) {
-    //   this.priceMax = this.price;
-    // }
-    // if (
-    //   this.price < this.defaultPriceMax &&
-    //   this.defaultPriceMax !== this.priceMax
-    // ) {
-    //   this.priceMax = this.defaultPriceMax;
-    // }
+    this.termRepaymentAmount = updatedTerm.detail.value;
+    this.termChange.emit(this.termRepaymentAmount);
   }
 }
